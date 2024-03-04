77a1810b615c37c90230ecba472c6464348783ae
Jonathan Vizcaino <jevizcaino@unimagdalena.edu.co>
2024-03-04 12:39:05 -0500

    (Backend): Metodo tokenizer separa en tokens solo números y creo dos clases que heredan de Token

--- lexer.py
+++ lexer.py
@@ -1,5 +1,6 @@
 class Lexer:
-    stopWords = [" "]
+    __stopWords = [" "]
+    __numbers =  "0123456789"
 
     def __init__(self, code:str):
         self.__code = code
@@ -12,26 +13,64 @@ class Lexer:
     
     def tokenizer(self):
         while self.__i < len(self.__code):
-            print(self.__i)
+            if self.__chr in Lexer.__numbers:
+                self.__token = self.__extractNumber()
+                self.__tokens.append(self.__token)
             self.__moveI()
     
+    def __extractNumber(self):
+        number = ""
+        while (self.__chr in Lexer.__numbers) and (self.__i < len(self.__code)):
+            number += self.__chr
+            self.__moveI()
+        return Integer(number, 'INTEGER')
+
     def __moveI(self):
         self.__i += 1
         if self.__i < len(self.__code):
             self.__chr = self.__code[self.__i]
 
     def __clean(self):
-        for stopWord in Lexer.stopWords:
+        for stopWord in Lexer.__stopWords:
             self.__code = self.__code.replace(stopWord,"")
 
 
     def getCode(self)->str:
         return self.__code
     
+    def getNumbers(self):
+        return Lexer.__numbers
+    
+    def getStopWords(self):
+        return Lexer.__stopWords
+    
+    def getTokens(self):
+        return self.__tokens
+    
+class Token:
+    def __init__(self,  tokenType, tokenValue):
+        self.__tokenType = tokenType
+        self.__tokenValue = tokenValue
+    
+    def getTokenType(self):
+        return self.__tokenType
+    
+    def getTokenValue(self):
+        return self.__tokenValue
+
+class Integer(Token):
+    pass
+
+class String(Token):
+    pass
+     
+    
 
 lex = Lexer("5      +     5")
 print(lex.getCode())
 print("------------------")
 lex.tokenizer()
+print("------------------")
+print(lex.getTokens())
 
 
