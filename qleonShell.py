09a7721d0e7a1e803afa851b6d5ed873b1936cb3
Jonathan Vizcaino <jevizcaino@unimagdalena.edu.co>
2024-03-05 14:46:11 -0500

    (BackEnd): Formalización de terminal, ahora es una clase más fácil de seguir expandiendo

--- qleonShell.py
+++ qleonShell.py
@@ -1,47 +1,50 @@
 from lexer import *
 
 
-HELPCOMMAND = "help()"
-EXITCOMMAND = "exit()" 
-VERSION = "1.0"
-DATECREATION = "Feb 4 2024, 1:41:20"
-HELPMESSAGE = f'Welcome to QLeon {VERSION} help utility!.\n\nWe are still working on finalizing the language.\n\nTo exit this help utility and return to the Lexer,\nenter "ex" or "exit".\n'
-INTERPRETEROUTPUT = "---> "
-HELPOUTPUT = "help()---> "
-EXITCOMMANDSHELP = ["ex", "exit"]
+class Shell:
+    __HELPCOMMAND = "help()"
+    __EXITCOMMAND = "exit()" 
+    __VERSION = "1.0"
+    __DATECREATION = "Feb 4 2024, 1:41:20"
+    __HELPMESSAGE = f'Welcome to QLeon {__VERSION} help utility!.\n\nWe are still working on finalizing the language.\n\nTo exit this help utility and return to the Lexer,\nenter "ex" or "exit".\n'
+    __INTERPRETEROUTPUT = "---> "
+    __HELPOUTPUT = "help()---> "
+    __EXITCOMMANDSHELP = ["ex", "exit"]
 
+    def __init__(self):
+        pass
 
-print(f"\nQLeon {VERSION} (v{VERSION}, {DATECREATION}) on win32")
-
-print(f'Write "{HELPCOMMAND}" for more information')
-
-def messageError(code) -> str:
-    error = f"Traceback (most recent call last):\nNameError: name '{code}' is not defined"
-    print(error)
-def messageUnk(code) -> str:
-    message = f"No QLeon documentation found for '{code}'\n"
-    print(message)
-            
-
-
-while True:
-    code = input(f"{INTERPRETEROUTPUT}")
-    if code == HELPCOMMAND:
-        print(HELPMESSAGE)
+    def qLeonShell(self):
+        print(f"\nQLeon {Shell.__VERSION} (v{Shell.__VERSION}, {Shell.__DATECREATION}) on win32")
+        print(f'Write "{Shell.__HELPCOMMAND}" for more information')
         while True:
-            code = input(f"{HELPOUTPUT}")
-            if code.lower() not in EXITCOMMANDSHELP:
-                messageUnk(code)
-                continue
+            code = input(f"{Shell.__INTERPRETEROUTPUT}")
+            if code == Shell.__HELPCOMMAND:
+                print(Shell.__HELPMESSAGE)
+                while True:
+                    code = input(f"{Shell.__HELPOUTPUT}")
+                    if code.lower() not in Shell.__EXITCOMMANDSHELP:
+                        self.__messageUnk(code)
+                        continue
+                    else:
+                        print("\nYou are leaving the help and returning to the QLeon lexer.")
+                        break
+            elif code == Shell.__EXITCOMMAND:
+                break  
             else:
-                print("\nYou are leaving the help and returning to the QLeon lexer.")
-                break
-    elif code == EXITCOMMAND:
-        break  
-    else:
-        lex = Lexer(code)
-        tokens = lex.tokenizer()
-        if len(tokens) == 0:
-            messageError(code)
-        else:
-            print(f"{tokens}")
+                lex = Lexer(code)
+                tokens = lex.tokenizer()
+                if len(tokens) == 0:
+                    self.__messageError(code)
+                else:
+                    print(f"{tokens}")
+
+    def __messageError(self, code) -> str:
+        error = f"Traceback (most recent call last):\nNameError: name '{code}' is not defined"
+        print(error)
+    def __messageUnk(self, code) -> str:
+        message = f"No QLeon documentation found for '{code}'\n"
+        print(message)
+
+myQLeon = Shell()
+myQLeon.qLeonShell()
